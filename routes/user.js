f167b60882389946e7b0ca9f6af7df15ea1f326e
Ryder-07 <kunals1209@gmail.com>
2025-12-18 00:42:24 +0530

    added basic signupn functionality

--- routes/user.js
+++ routes/user.js
@@ -3,9 +3,19 @@ const {userModel} = require("../db");
 const userRouter = Router();
 
 
-    userRouter.post("/signup", function(req,res){
+    userRouter.post("/signup",async function(req,res){
+        const{email,password,firstname,lastName} = req.body; // TODO: adding zod validation
+        // TODO: hash the password sp plaintext pw is not stored in DB
+
+        // TODO: put inside a try catch block
+        await userModel.create({
+            email,
+            passowrd,
+            firstname,
+            lastName
+        })
         res.json({
-            message: "signup endpoint"
+            message: "signup succeded"
         })
     })
 
