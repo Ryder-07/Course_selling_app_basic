81c5999bfc1076d9eeb4046b053ba1ab3a6a1dd4
Ryder-07 <kunals1209@gmail.com>
2025-12-20 18:18:11 +0530

    added courses.js endpoints for purchase and preview

--- routes/courses.js
+++ routes/courses.js
@@ -1,18 +1,31 @@
 const {Router} = require("express");
 const courseRouter = Router();
+const { userMiddleware} = require("../middleware/user");
+const {purchaseModel, courseModel} = require("../db");
+const { $ZodAsyncError } = require("zod/v4/core");
 
 
-    courseRouter.post("/purchase", function(req,res){
+    courseRouter.post("/purchase",userMiddleware,async function(req,res){
+        const userId = req.userId;
+        const courseId = req.courseId;
+
+        await purchaseModel.create({
+            userId,
+            courseId
+        })
+
+
         res.json({
-            message: "signup endpoint"
+            message: "You have successfully bought the course"
         })
     })
 
 
 
-    courseRouter.get("/preview", function(req,res){
+    courseRouter.get("/preview",async function(req,res){
+        const courses = await courseModel.find({});
         res.json({
-            message: "signup endpoint"
+            courses 
         })
     })
 
